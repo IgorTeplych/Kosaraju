7ae0e174fdc7dcac3c92c6ff25298f12ff9dc50f
Igor Teplych <igorvrt@mail.ru>
2023-01-11 01:14:11 +0600

    Добавил тестовый проект

---
+++ Kosaraju.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Kosaraju.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
